f2468a882750ad6ac0d057cce1cb46ee7eb2e72a
seimon-m <simimueller@hotmail.com>
2020-02-18 17:56:36 +0100

    Initial commit

---
+++ src/main/java/ch/hslu/sw1/Aha.java
@@ -0,0 +1,42 @@
+package ch.hslu.sw1;
+
+public class Aha {
+
+    public static void main(String[] args) {
+        task(5);
+    }
+
+    public static void task(final int n) {
+        task1();
+        task1();
+        task1();
+        task1();    // T ~ 4
+
+        for (int i = 0; i < n; i++) {   // äussere Schleife: n-mal
+
+            task2();
+            task2();
+            task2();    // T ~ n · 3
+
+            for (int j = 0; j < n; j++) {   // innerer Schleife: n-mal
+
+                task3();    // T ~ n · n· 2
+                task3();
+            }
+
+        }
+
+    }
+
+    public static void task1() {
+
+    }
+
+    public static void task2() {
+
+    }
+
+    public static void task3() {
+
+    }
+}
