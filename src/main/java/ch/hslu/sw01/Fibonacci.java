5f5ba758ba8d4eb8968567660c392c4e2e4c1aac
seimon-m <simimueller@hotmail.com>
2020-06-05 09:48:36 +0200

    add fiboRec2 and time measurement, some refactoring

--- src/main/java/ch/hslu/sw01/Fibonacci.java
+++ src/main/java/ch/hslu/sw01/Fibonacci.java
@@ -7,43 +7,63 @@ public class Fibonacci {
 
     private static final Logger LOG = LogManager.getLogger(Fibonacci.class);
 
+    static long storage[];
+
     public static void main(final String[] args) {
-        for (int i = 1; i <= 7; i++) {
-            LOG.info(fiboRec1(i));
-        }
-        fiboIter(7);
-        LOG.info(fiboRec2(10));
+        int n = 50;
+
+        long time1 = System.currentTimeMillis();
+        LOG.info(fiboIter(n));
+        long time2 = System.currentTimeMillis();
+        LOG.info("Fibonacci Iterativ: " + (time2 - time1) + "ms");
+
+        time1 = System.currentTimeMillis();
+        LOG.info(fiboRec1(n));
+        time2 = System.currentTimeMillis();
+        LOG.info("Fibonacci Rekursiv: " + (time2 - time1) + "ms");
+
+        time1 = System.currentTimeMillis();
+        LOG.info(fiboRec2(n));
+        time2 = System.currentTimeMillis();
+        LOG.info("Fibonacci Rekursiv mit Speicher: " + (time2 - time1) + "ms");
     }
 
-    public static void fiboIter(int n) {
-        int z0 = 0;
-        int z1 = 1;
-        int sum = 0;
-
-        for (int i = 0; i <= n; i++) {
-            LOG.info(z0);
-            sum = z0 + z1;
-            z0 = z1;
-            z1 = sum;
+    public static long fiboIter(int n) {
+        if (n > 0) {
+            long aktuelle = 1, temp = 1, vorherige = 0;
+            for (long i = 1; i < n; i++) {
+                temp = aktuelle;
+                aktuelle = aktuelle + vorherige;
+                vorherige = temp;
+            }
+            return aktuelle;
+        } else {
+            return 0;
         }
     }
 
-
-    private static int fiboRec1(int n) {
+    private static long fiboRec1(int n) {
         if (n == 1 || n == 2) { // Rekursionbasis
             return 1;
         }
         return fiboRec1(n - 1) + fiboRec1(n - 2); // Rekursionvorschrift
     }
 
-    /**
-     * Berechnet den Fibonacci Wert für n.
-     *
-     * @param n für die Fibonacci Berechnung.
-     * @return Resultat der Fibonacci Berechnung.
-     */
-    public static int fiboRec2(final int n) {
-        return n > 1 ? fiboRec2(n - 1) + fiboRec2(n - 2) : n;
+    public static long fiboRec2(final int n) {
+        storage = new long[n + n];
+        return fiboHelper(n);
+    }
+
+    private static long fiboHelper(int n) {
+        if (storage[n] != 0) {
+            return storage[n];
+        } else if (n < 2) {
+            storage[n] = n;
+            return storage[n];
+        } else {
+            storage[n] = fiboHelper(n - 1) + fiboHelper(n - 2);
+        }
+        return storage[n];
     }
 }
 
