0ff3bc3210344b2ce8257173696223d827a0ee7b
Simon <simimueller@hotmail.com>
2020-06-06 18:20:06 +0200

    better performance measurement and some refactoring

--- src/main/java/ch/hslu/sw04/MainQueue.java
+++ src/main/java/ch/hslu/sw04/MainQueue.java
@@ -1,31 +1,44 @@
 package ch.hslu.sw04;
 
-import ch.hslu.sw02.Queue;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ConcurrentLinkedDeque;
 
 
 public class MainQueue {
-    int size = 1000000;
+    int size = 10000000;
     String[] array = new String[size];
+    static List<Long> timeList = new ArrayList<>();
 
     private static final Logger LOG = LogManager.getLogger(MainQueue.class);
 
     public static void main(String[] args) {
 
-        int measurements = 20;
+        int measurements = 10;
         MainQueue main = new MainQueue();
         LOG.info("JCF ConcurrentLinkedDeque measurements:");
         for (int i = 0; i < measurements; i++) {
             LOG.info(main.jcfConcurrentLinkedDeque());
         }
+        int sum = 0;
+        for (int i = 1; i < timeList.size(); i++) {
+            sum += timeList.get(i);
+        }
+        LOG.info("JCF ConcurrentLinkedDeque average time: " + sum / (measurements - 1) + "ms");
+        timeList.clear();
 
         LOG.info("MyQueue measurements:");
         for (int i = 0; i < measurements; i++) {
             LOG.info(main.myQueue());
         }
+        sum = 0;
+        for (int i = 1; i < timeList.size(); i++) {
+            sum += timeList.get(i);
+        }
+        LOG.info("MyQueue average time: " + sum / (measurements - 1) + "ms");
     }
 
     public long jcfConcurrentLinkedDeque() {
@@ -36,10 +49,11 @@ public class MainQueue {
         ConcurrentLinkedDeque<String> jcfQueue = new ConcurrentLinkedDeque<>();
 
         long time1 = System.currentTimeMillis();
-        for (int i = 0; i < array.length; i++) {
-            jcfQueue.add(array[i]);
+        for (String s : array) {
+            jcfQueue.add(s);
         }
         long time2 = System.currentTimeMillis();
+        timeList.add(time2 - time1);
         return time2 - time1;
     }
 
@@ -51,10 +65,11 @@ public class MainQueue {
         MyQueue myQueue = new MyQueue(size);
 
         long time1 = System.currentTimeMillis();
-        for (int i = 0; i < array.length; i++) {
-            myQueue.add(array[i]);
+        for (String s : array) {
+            myQueue.add(s);
         }
         long time2 = System.currentTimeMillis();
+        timeList.add(time2 - time1);
         return time2 - time1;
     }
 }
--- src/main/java/ch/hslu/sw04/MainStack.java
+++ src/main/java/ch/hslu/sw04/MainStack.java
@@ -3,27 +3,41 @@ package ch.hslu.sw04;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 public class MainStack {
-    int size = 1000000;
+    int size = 10000000;
     String[] array = new String[size];
+    static List<Long> timeList = new ArrayList<>();
 
     private static final Logger LOG = LogManager.getLogger(MainStack.class);
 
     public static void main(String[] args) {
 
-        int measurements = 20;
+        int measurements = 10;
         MainStack main = new MainStack();
-        LOG.info("JavaStack measurements:");
+        LOG.info("JCF Stack measurements:");
         for (int i = 0; i < measurements; i++) {
             LOG.info(main.javaStack());
         }
+        int sum = 0;
+        for (int i = 1; i < timeList.size(); i++) {
+            sum += timeList.get(i);
+        }
+        LOG.info("JCF Stack average time: " + sum / (measurements - 1) + "ms");
+        timeList.clear();
 
         LOG.info("MyStack measurements:");
         for (int i = 0; i < measurements; i++) {
             LOG.info(main.myStack());
         }
+        sum = 0;
+        for (int i = 1; i < timeList.size(); i++) {
+            sum += timeList.get(i);
+        }
+        LOG.info("MyStack average time: " + sum / (measurements - 1) + "ms");
     }
 
     public long javaStack() {
@@ -34,10 +48,11 @@ public class MainStack {
         Stack<String> javaStack = new Stack<>();
 
         long time1 = System.currentTimeMillis();
-        for (int i = 0; i < array.length; i++) {
-            javaStack.push(array[i]);
+        for (String s : array) {
+            javaStack.push(s);
         }
         long time2 = System.currentTimeMillis();
+        timeList.add(time2 - time1);
         return time2 - time1;
     }
 
@@ -49,10 +64,11 @@ public class MainStack {
         ch.hslu.sw02.Stack myStack = new ch.hslu.sw02.Stack(size);
 
         long time1 = System.currentTimeMillis();
-        for (int i = 0; i < array.length; i++) {
-            myStack.push(array[i]);
+        for (String s : array) {
+            myStack.push(s);
         }
         long time2 = System.currentTimeMillis();
+        timeList.add(time2 - time1);
         return time2 - time1;
     }
 }
