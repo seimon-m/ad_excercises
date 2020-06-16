4aeab6014e842975f486ac8866510fe4caaef43f
Simon <simimueller@hotmail.com>
2020-06-16 10:20:13 +0200

    maybe a fix for optimalMismatch. But is still slower than quicksearch

--- src/main/java/ch/hslu/sw14/Quicksearch.java
+++ src/main/java/ch/hslu/sw14/Quicksearch.java
@@ -4,21 +4,48 @@ import org.apache.logging.log4j.Logger;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Quicksearch {
 
     private static final Logger LOG = LogManager.getLogger(Quicksearch.class);
+    private static List<Long> time = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
-        String a = readFileAsString("/Users/simon/DasBuchDerBilder.txt");
-        long time1 = System.currentTimeMillis();
-        int result = Quicksearch.quickSearch(a, "Ouvertüre");
-        long time2 = System.currentTimeMillis();
-        LOG.info("Pattern at index: " + result + " | Time: " + (time2 - time1) + "ms");
+        int rounds = 10;
+        for (int i = 0; i < rounds; i++) {
+            String a = readFileAsString("/Users/simon/Documents/GitHub/ad_excercises/src/main/java/ch/hslu/sw14/DasBuchDerBilder.txt");
+            long time1 = System.currentTimeMillis();
+            long result = Quicksearch.quickSearch(a, "HalloVelo");
+            long time2 = System.currentTimeMillis();
+            time.add(time2 - time1);
+            LOG.info("Pattern at index: " + result + " | Time: " + (time2 - time1) + "ms");
+        }
+        long sum = 0;
+        for (Long t : time) {
+            sum += t;
+        }
+        LOG.info("Quicksort average time: " + sum / time.size() + "ms");
+
+        time.clear();
+        for (int i = 0; i < rounds; i++) {
+            String a = readFileAsString("/Users/simon/Documents/GitHub/ad_excercises/src/main/java/ch/hslu/sw14/DasBuchDerBilder.txt");
+            long time1 = System.currentTimeMillis();
+            long result = Quicksearch.optimalMismatch(a, "HalloVelo");
+            long time2 = System.currentTimeMillis();
+            time.add(time2 - time1);
+            LOG.info("Pattern at index: " + result + " | Time: " + (time2 - time1) + "ms");
+        }
+        sum = 0;
+        for (Long t : time) {
+            sum += t;
+        }
+        LOG.info("OptimalMismatch average time: " + sum / time.size() + "ms");
     }
 
     public static String readFileAsString(String Book)throws Exception{
-        String a = "";
+        String a;
         a = new String(Files.readAllBytes(Paths.get(Book)));
         return a;
     }
@@ -29,10 +56,10 @@ public class Quicksearch {
      * @param a Zeichenkette, die durchsucht wird.
      * @return Index der Fundstelle oder -1, falls Pattern in a nicht gefunden wurde.
      */
-    public static int quickSearch(final String a, final String p) {
+    public static long quickSearch(final String a, final String p) {
         final int n = a.length();   // Length of text
         final int m = p.length();   // Length of pattern
-        final int range = 256;      // -> ASCII-Range
+        final int range = 56536;      // -> Unicode-Range
         final int[] shift = new int[range];
 
         // init shift-array
@@ -69,12 +96,11 @@ public class Quicksearch {
         }
     }
 
-    public static int optimalMismatch(final String a, final String p) {
-        final int n = a.length();
-        final int m = p.length();
-        final int range = 256; // -> ASCII-Range
+    public static long optimalMismatch(final String a, final String p) {
+        final int n = a.length();   // Length of text
+        final int m = p.length();   // Length of pattern
+        final int range = 56536;      // -> Unicode-Range
         final int[] shift = new int[range];
-        final char[] pArray= p.toCharArray();
 
         // init shift-array
         for (int i = 0; i < range; i++) {
@@ -87,23 +113,23 @@ public class Quicksearch {
         }
 
         // search
-        int i = 0;      // index to string
-        int j = 0;      // index to pattern p
+        int i = 0;          // index to string
+        int j = 0;          // index to pattern p
         do {
             if (a.charAt(i + j) == p.charAt(j)) { // match
                 j++;
             } else { // mismatch
                 if ((i + m) < n) { // a.charAt(i1+m) is not outside a
-                    i += shift[a.charAt(i + m)]; // jump forward
-                    swapElement(pArray, j);
+                    int elementIndex = a.charAt(i + m); // jump forward
+                    i += shift[a.charAt(i + m)];
                     j = 0;
+                    reorganiseShift(shift, elementIndex);
                 } else {
                     break; // (mismatch) && (no shift is possible)
                 }
             }
         }
-        while ((j < m) && ((i + m) <= n));
-        // (pattern p not found) && (end of a not reached)
+        while ((j < m) && ((i + m) <= n));  // (pattern p not found) && (end of a not reached)
         if (j == m) {
             return i; // pattern found
         } else {
@@ -111,10 +137,14 @@ public class Quicksearch {
         }
     }
 
-    private static void swapElement(char[] array, int element) {
-        char temp = array[element];
-        array[element] = array[0];
-        array[0] = temp;
+
+    private static void reorganiseShift(int[] shift, int index) { // Mismatch-Element an erste Stelle im shift-Array
+        int old = shift[0];
+        shift[0] = shift[index];
+        for (int i = 1; i < index; i++) {
+            shift[i] = old;
+            old = shift[i + 1];
+        }
     }
 }
 
--- src/test/java/ch/hslu/sw14/QuicksearchTest.java
+++ src/test/java/ch/hslu/sw14/QuicksearchTest.java
@@ -30,10 +30,46 @@ class QuicksearchTest {
         String p = "aaaaaaaaaaaaaaaaaaba";
         assertEquals(-1, Quicksearch.quickSearch(a, p));
     }
+
     @Test
     void quickSearch4() {
         String a = "aaaabacabacabacabacabacabac";
         String p = "abac";
         assertEquals(3, Quicksearch.quickSearch(a, p));
+    }
+
+    @Test
+    void optimalMismatch() {
+        String a = "abcdefghjiabaabaaabac";
+        String p = "abac";
+        assertEquals(17, Quicksearch.optimalMismatch(a, p));
+    }
+
+    @Test
+    void optimalMismatch1() {
+        String a = "abcdefghjiabaabaaabaac";
+        String p = "abac";
+        assertEquals(-1, Quicksearch.optimalMismatch(a, p));
+    }
+
+    @Test
+    void optimalMismatch2() {
+        String a = "acdefghijklmnopqrst";
+        String p = "bbbb";
+        assertEquals(-1, Quicksearch.optimalMismatch(a, p));
+    }
+
+    @Test
+    void optimalMismatch3() {
+        String a = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        String p = "aaaaaaaaaaaaaaaaaaba";
+        assertEquals(-1, Quicksearch.optimalMismatch(a, p));
+    }
+
+    @Test
+    void optimalMismatch4() {
+        String a = "aaaabacabacabacabacabacabac";
+        String p = "abac";
+        assertEquals(3, Quicksearch.optimalMismatch(a, p));
     }
 }
